2d0aa6168204f6933e5b9c458d549f155d8f061e
JaimeValleZ <jaime.valle@cun.edu.co>
2025-09-25 12:38:24 -0500

    Componente balanceador

---
+++ src/app/pages/balanceador/balanceador.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balanceador',
+  imports: [],
+  templateUrl: './balanceador.html',
+  styleUrl: './balanceador.css'
+})
+export class Balanceador {
+
+}
