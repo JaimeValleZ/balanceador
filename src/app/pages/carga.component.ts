09639b5a4425c6e3e59bed8a48db12868f53000e
Edz <kevin.sanchezv@cun.edu.co>
2025-09-25 22:27:56 -0500

    Apartado grafico basico y visualizacion en tabla

--- src/app/pages/carga.component.ts
+++ src/app/pages/carga.component.ts
@@ -18,24 +18,17 @@ interface Producto {
   imports: [CommonModule, FormsModule]
 })
 export class CargaComponent {
-  productos: Producto[] = [{ id: 0, name: '', price: 0 }];
+    producto: Producto = { id: 0, name: '', price: 0 };
   mensaje = '';
 
   constructor(private http: HttpClient) {}
 
-  agregarFila() {
-    this.productos.push({ id: 0, name: '', price: 0 });
-  }
 
-  eliminarFila(i: number) {
-    this.productos.splice(i, 1);
-  }
-
-  enviar() {
-    this.http.post('http://localhost:8080/api/products/bulk', this.productos)
-      .subscribe({
-        next: () => this.mensaje = '¡Productos agregados exitosamente!',
-        error: () => this.mensaje = 'Error al agregar productos.'
-      });
+    enviar() {
+      this.http.post('http://localhost:8080/api/products', this.producto)
+        .subscribe({
+          next: () => this.mensaje = '¡Producto agregado exitosamente!',
+          error: () => this.mensaje = 'Error al agregar producto.'
+        });
   }
 }
