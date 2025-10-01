46b2539d751e5f645c4f20007ffedae0e92aa235
JaimeValleZ <jaime.valle@cun.edu.co>
2025-09-30 19:29:34 -0500

    Modificacion de environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'https://balanceador-backend-3.onrender.com',   // Render
-    'https://balanceador-backend-production.up.railway.app' // Railway
+    'https://balanceador-backend-3.onrender.com/api',   // Render
+    'https://balanceador-backend-production.up.railway.app/api' // Railway
   ]};
