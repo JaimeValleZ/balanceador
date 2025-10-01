ee8a539404f2ce18bab233e3e22e7c7a5cca3d69
JaimeValleZ <jaime.valle@cun.edu.co>
2025-09-30 21:17:44 -0500

    Modificacion de la ruta para el nuevo backend en render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'https://balanceador-backend-3.onrender.com/api',   // Render
+    'https://balanceador-backend-4.onrender.com/api',   // Render
     'https://balanceador-backend-production.up.railway.app/api' // Railway
   ]};
