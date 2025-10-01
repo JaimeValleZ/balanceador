3ee2fc695565dae1e4cc91db7e384749684be571
Juanlink22 <juan.carrilloq@cun.edu.co>
2025-10-01 16:10:57 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'https://balanceador-backend-4.onrender.com/api',   // Render
-    'https://balanceador-backend-production.up.railway.app/api', // Railway
-    'https://roasted-kathie-cun-f6b631be.koyeb.app/api'// Koyeb
-  ]};
+    'alert-layne-corporaccion-dc1b7acc.koyeb.app/api' // solo Koyeb
+  ]
+};
