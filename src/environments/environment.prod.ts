d097dd84a6e9487afc34139753f10a1f6a0901e6
JaimeValleZ <jaime.valle@cun.edu.co>
2025-09-30 22:10:40 -0500

    Agregar ruta de Koyeb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrls: [
     'https://balanceador-backend-4.onrender.com/api',   // Render
-    'https://balanceador-backend-production.up.railway.app/api' // Railway
+    'https://balanceador-backend-production.up.railway.app/api', // Railway
+    'https://roasted-kathie-cun-f6b631be.koyeb.app/api'// Koyeb
   ]};
