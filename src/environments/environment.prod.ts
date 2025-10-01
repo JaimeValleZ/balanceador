ba586b3538be1479750f9fa641b490d2e9021665
Juanlink22 <juan.carrilloq@cun.edu.co>
2025-10-01 16:17:31 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'alert-layne-corporaccion-dc1b7acc.koyeb.app/api' // solo Koyeb
+    'https://alert-layne-corporaccion-dc1b7acc.koyeb.app//api' // solo Koyeb
   ]
 };
