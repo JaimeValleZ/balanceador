d3c159d8cf15d2a94737defe6fdff8e504952f92
JaimeValleZ <168594287+JaimeValleZ@users.noreply.github.com>
2025-11-13 21:17:56 -0500

    Update production API URL in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'https://academic-meerkat-66-cun-66-b41a70fa.koyeb.app/api' // solo Koyeb
+    'https://only-jinny-cun-7b0efbb1.koyeb.app/api' // solo Koyeb
   ]
 };
